4cf25ea6adccd1c962010ff7b4b544c96a013794
JayAhr <sgjohann@gmail.com>
2020-10-03 20:08:58 +0200

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+let mix = require('laravel-mix');
+
+const tailwindcss = require('tailwindcss');
+
+mix.js('src/js/app.js', 'public/js/')
+   .sass('src/sass/app.scss', 'public/css')
+   .options({ 
+       processCssUrls: false,
+       postCss: [tailwindcss('./tailwind.config.js')]
+    });
